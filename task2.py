a34b1fc749a45c8e458ce1e235d440b38ed58476
hannausatenko <hanna.usatenko@gmail.com>
2024-05-18 16:12:19 +0100

    init

---
+++ task2.py
@@ -0,0 +1,15 @@
+import random
+
+
+def get_numbers_ticket(min, max, quantity):
+    if not (1 <= min <= max <= 1000):
+        return []
+    if not (1 <= quantity <= (max - min + 1)):
+        return []
+    numbers = random.sample(range(min, max + 1), quantity)
+    numbers.sort()
+
+    return numbers
+
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
